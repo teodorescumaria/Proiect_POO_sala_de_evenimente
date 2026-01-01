dfb360aba6b16e5eb015904c7c786005875e06fa
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-01 21:14:15 +0100

    Update Bilet.cpp

--- src/Bilet.cpp
+++ src/Bilet.cpp
@@ -1,1 +1,21 @@
+#include "Bilet.h"
 
+Bilet::Bilet(cont Persoana& persoana, const Loc& loc, double pretBaza, const std::string& tipEveniment) : persoana(persoana), loc(loc), pretBaza(pretBaza), tipEveniment(tipEveniment) {}
+
+const Persoana& Bilet::getPersoana() const {
+  return persoana;
+}
+
+const Loc& Bilet::getLoc() const {
+  return loc;
+}
+
+double Bilet::getPretDeBaza() const {
+  return pretDeBaza;
+}
+const std::string& Bilet::getTipEveniment() const {
+  return tipEveniment;
+}
+std::string Bilet::descriere() const {
+  return "Bilet pentru " + persoana.getNume() + " la " + tipEveniment;
+}
