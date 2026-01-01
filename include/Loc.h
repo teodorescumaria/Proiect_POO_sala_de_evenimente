bf2fdeda5f8ff98f10d448668f38216227f2a3f1
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-01 20:25:46 +0100

    Update Loc.h

--- include/Loc.h
+++ include/Loc.h
@@ -1,5 +1,5 @@
-#ifndef LOC_h
-#define LOC_h
+#ifndef LOC_H
+#define LOC_H
 
 class Loc
 {
@@ -9,7 +9,7 @@ class Loc
     bool esteVip;
     bool esteOcupat;
   public:
-    loc(int rand = 1, int coloana = 1, bool esteVip = false, bool esteOcupat = false);
+    Loc(int rand = 1, int coloana = 1, bool esteVip = false, bool esteOcupat = false);
     int getRand() const;
     int getColoana() const;
     bool esteVIP() const;
