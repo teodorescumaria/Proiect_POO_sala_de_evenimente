bf4f65a5b538d61ce9db5c6df392dc8bf91ab73a
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-01 20:15:00 +0100

    Update sala.h

--- include/sala.h
+++ include/sala.h
@@ -9,7 +9,20 @@ class sala
   private:
     std::string tip_sala;
     int randuri;
-    int 
+    int locuriPeRand;
+    std::vector<loc> locuri;
+  public:
+    sala(const std::string& tip_sala = "", int randuri = 1, int locuriPeRand = 1);
+    const std::string& getTipSala() const;
+    int getRanduri() const;
+    int getLocuriPeRand() const;
+
+//marcam un rand ca VIP
+    void initializareLocuriVIP(int randVIP);
+    bool esteLocVIP(int rand, int coloana);
+    loc& getLoc(int rand, int coloana);
+    void afisareLocuri() const;
+};
     
 
 #endif
