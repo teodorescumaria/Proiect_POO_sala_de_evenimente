a3a06565554979c02bb9c4f06ef4db19aef96c7d
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-01 19:35:34 +0100

    Update persoana.h

--- include/persoana.h
+++ include/persoana.h
@@ -1,2 +1,20 @@
-#ifndef
-#
+#ifndef Persoana_h
+#define Persona_h
+
+#include <string>
+
+class Persoana 
+{
+  private: 
+    std::string nume;
+    int anulNasterii;
+  public:
+    Persoana(const std::string& nume = "", int anulNasterii = 2000);
+    const std::string& getNume() const;
+    int getvarsta() const;
+    bool esteCopil() const;
+    bool esteSenior() const;
+};
+
+
+#endif
