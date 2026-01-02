32b461af2f9b288d6005e1b86483675efc05b3ca
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 10:31:23 +0100

    Create BiletStandard.cpp

---
+++ src/BiletStandard.cpp
@@ -0,0 +1,11 @@
+#include "BiletStandard.h"
+
+BiletStandard::BiletStandard(const Persoana& persoana, const Loc& loc, double pretDeBaza, const std::string& tipEveniment) : Bilet(persoana, loc, pretDeBaza, tipEveniment) {}
+
+double BiletStandard::calculeazaPret() const {
+  return pretDeBaza;
+}
+
+std::string BiletStandard::descriere() const {
+  return "Bilet STANDARD | " + Bilet::descriere() + " | Pret: " + std::to_string(calculeazaPret());
+}
