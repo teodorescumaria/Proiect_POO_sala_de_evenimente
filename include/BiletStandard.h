d51b97b6b34b0851e7d7dc98212f06afbebdaf47
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 10:22:04 +0100

    Create BiletStandard.h

---
+++ include/BiletStandard.h
@@ -0,0 +1,12 @@
+#ifndef BILET_STANDARD_H
+#define BILET_STANDARD_H
+
+#include "Bilet.h"
+
+class BiletStandard : public Bilet
+{
+  BiletStandard(const Persoana& persoana, const Loc& loc, double pretDeBaza, const std::string& tipEveniment);
+  double calculeazaPret() const override;
+  std::string descriere() const override;
+};
+#endif
