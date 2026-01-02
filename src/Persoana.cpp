96cba9de20375a2a0bc48fc14e7de0e61a4c5663
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 09:42:23 +0100

    Create Persoana.cpp

---
+++ src/Persoana.cpp
@@ -0,0 +1,18 @@
+#include "Persoana.h"
+
+Persoana::Persoana(const std::string& nume, int anulNasterii) : nume(nume), anulNasterii(anulNasterii) {}
+
+const std::string& Persoana::getNume() const {
+  return nume;
+}
+
+int Persoana::getVarsta() const {
+  int anCurent = 2026;
+  return anCurent - anulNasterii;
+}
+bool Persoana::esteCopil() const {
+  return getVarsta() < 12;
+}
+bool Persoana::esteSenior() const {
+  return getVarsta() >= 65;
+}
