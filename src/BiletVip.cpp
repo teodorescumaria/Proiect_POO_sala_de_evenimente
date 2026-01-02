07fcd89cf00b516992083dc9d7e9d44a55bfb2e6
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 11:11:06 +0100

    Create BiletVip.cpp

---
+++ src/BiletVip.cpp
@@ -0,0 +1,25 @@
+#include "BiletVip.h"
+#include <string>
+
+BiletVip::BiletVip(const Persoana& persoana, const Loc& loc, double preDeBaza, const std::string& tipEveniment, bool arePopcorn, bool areBautura) : Bilet(persoana, loc, pretDeBaza, tipEveniment), arePopcorn(arePopcorn), areBautura(areBautura) {}
+
+double BiletVip::calculeazaPret() const {
+  double = pretDeBaza*1.3;
+  return pret;
+}
+
+std::string BiletVip::descriere() const {
+  std::string extrauri = " (extrauri: ";
+  if(arePopcorn) {
+    extrauri += "popcorn";
+  }
+  if(areBautura) {
+    if(arePopcorn) 
+      extrauri += ", ";
+    extrauri += "bautura";
+  }
+  if(!arePopcorn && !areBautura) {
+    extrauri += "fara nimic extra";
+  }
+  extrauri += ")";
+  return "Bilet VIP | " + Bilet::descriere() + extrauri + " | Pret VIP: " + std::to_string(calculeazaPret());
