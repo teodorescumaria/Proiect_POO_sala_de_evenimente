c8be5315e58a5a6be4c27533e73419aaaa7244a8
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 09:52:45 +0100

    Create Loc.cpp

---
+++ src/Loc.cpp
@@ -0,0 +1,24 @@
+#include "Loc.h"
+
+Loc::Loc(int rand, int coloana, bool esteVip, bool ocupat) : rand(rand), coloana(coloana), esteVip(esteVip), ocupat(ocupat) {}
+
+int Loc::getRand() const {
+  return rand;
+}
+int Loc::getColoana() const {
+  return coloana;
+}
+bool Loc::esteVIP() const {
+  return esteVip;
+}
+bool Loc::esteOcupat() const {
+  return ocupat;
+}
+
+void Loc::ocupa() {
+  ocupat = true;
+}
+void Loc::elibereaza()
+{
+  ocupat = false;
+}
