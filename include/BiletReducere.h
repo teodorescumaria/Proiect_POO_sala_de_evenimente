641c9fb2049c9952ab15bb2b3fd3c8721151b497
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 10:42:03 +0100

    Create BiletReducere.h

---
+++ include/BiletReducere.h
@@ -0,0 +1,13 @@
+#ifndef BILET_REDUCERE_H
+#define BILET_REDUCERE_H
+
+#include "Bilet.h"
+
+class BiletReducere : public Bilet
+{
+  public:
+    BiletReducere(const Persoana& persoana, const Loc& loc, double pretDeBaza, const std::string& tipEveniment);
+    double calculeazaPret() const override;
+    std::string descriere() const override;
+};
+#endif
