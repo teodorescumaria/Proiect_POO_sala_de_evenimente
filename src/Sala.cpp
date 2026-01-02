35b57e68fbabdabd4768480109cb4d7f4f703a2a
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 11:56:33 +0100

    Update Sala.cpp

--- src/Sala.cpp
+++ src/Sala.cpp
@@ -1,6 +1,61 @@
 #include "Sala.h"
+#include <iostream>
 
-class Sala {
+Sala::Sala(const std::string& tip_sala, int randuri, int locuriPeRand) : tip_sala(tip_sala), randuri(randuri), locuriPeRand(locuriPeRand) {
+  locuri.reserve(randuri*locuriPeRand);
+  for(int r=1; r<=randuri; ++r) {
+    for(int c = 1; c<= locuriPeRand; ++c) {
+      locuri.emplace_back(r, c, false, false);
+    }
+  }
+}
 
+const std::string& Sala::getTipSala() const {
+  return tip_sala;
+}
+int Sala::getRanduri() const{
+  return randuri;
+}
+int Sala::getLocuriPeRand() const {
+  return locuriPeRand;
+}
+void Sala::initializareLocuriVip(int randVip) {
+  for(auto &loc : locuri) {
+    if(loc.getRand() == randVip) {
+      loc = Loc(loc.getRand(), loc.getColoana(), true, loc.esteOcupat());
+    }
+  }
+}
 
-};
+bool Sala::esteLocVip(int rand, int coloana) {
+  if(rand < 1 || rand > randuri || coloana < 1 || coloana > locuriPeRand) {
+    return false;
+  }
+  int index = (rand-1)* locuriPeRand +(coloana - 1);
+  return locuri[index].esteVip();
+}
+
+Loc& Sala::getLoc(int rand, int coloana) {
+  int index = (rand-1) * locuriPeRand + (coloana-1);
+  return locuri[index];
+}
+
+void Sala::afisareLocuri() const {
+  std::cour << "SALA (" <<tip_sala<<") are "<<randuri<<" randuri si "<<locuriPeRand<<" locuri pe rand. \n";
+  for(int r=1; r<=randuri, ++r) {
+    for(in c = 1; c<=locuriPeRand; ++c) {
+      int index(r-1)*locuriPeRand+(c-1);
+      const Loc& loc = locuri[index];
+      char simbol = '-';
+      if(loc.esteVip()) {
+        simbol = 'V';
+      }
+      if(loc.esteOcupat()) { 
+        simbol = 'X';
+      }
+      std::cout<<simbol<<' ';
+    }
+    std::cout<<"\n";
+  }
+}
+
