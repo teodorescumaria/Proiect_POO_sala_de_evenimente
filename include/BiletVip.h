58688b381a7fdc1246851bd966799acde6d3fa06
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 11:02:13 +0100

    Update BiletVip.h

--- include/BiletVip.h
+++ include/BiletVip.h
@@ -1,3 +1,15 @@
 #ifndef BILET_VIP_H
 #define BILET_VIP_H
 #include "Bilet.h"
+
+class BiletVip : public Bilet {
+  private:
+    bool arePopcorn;
+    bool areBautura;
+  public:
+    BiletVip(const Persoana& persoana, const Loc& loc, const std::string& tipEveniment, bool arePopcorn = true, bool areBautura = true);
+    double calculeazaPret() const override;
+    std::string descriere() const override;
+};
+
+#endif
