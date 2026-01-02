8ec9e7306f2f0a1e1453cf783518939bed19860c
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-02 13:17:43 +0100

    Update Eveniment.h am adaugat destructor virtual si getteri
    am adăugat destructorul virtual
    am adaugat getteri pentru: nume, tip, data, pretBaza, durataMinute, sala

--- include/Eveniment.h
+++ include/Eveniment.h
@@ -19,6 +19,15 @@ public:
   Eveniment(const std::string& nume, const std::string& tip, const std::string& data, double pretBaza, int DurataMinute, const Sala& sala);
   Eveniment(const std::string& nume, const std::string& tip, const std::string& data, double pretBaza, const Sala& sala);
   Eveniment(const Eveniment& ev);
+  virtual ~Eveniment();
 
-}
+  const std::string& getNume() const;
+  const std::string& getTip() const;
+  const std::string& getData() const;
+  double getPretBAza() const;
+  int getDurataMinute() const;
+  const Sala& getsala() const;
+  Sala& getSala();
+
+};
 #endif
