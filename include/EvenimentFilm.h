248460d4cac4a037d535aaf15fd7a1b8d4c93577
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-03 10:42:16 +0100

    Create EvenimentFilm.h am creat clasa si am adăugat constructorii

---
+++ include/EvenimentFilm.h
@@ -0,0 +1,16 @@
+#ifndef EVENIMENT_FILM_H
+#define EVENIMENT_FILM_H
+
+#include "Eveniment.h"
+#include <string>
+
+class EvenimentFilm : public Eveniment {
+private:
+    std::string regizor;
+    std::string rating;
+public:
+    EvenimentFilm();
+    EvenimentFilm(const std::string& nume, const std::string& data, double pretBaza, const Sala& sala, const std::string& regizor, const std::string& raiting);
+    EvenimentFilm(const EvenimentFilm& other);
+};
+#endif
