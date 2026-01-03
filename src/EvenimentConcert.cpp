3fee6d109fbc673bf90f4bac4d395d37fdfbf324
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-03 10:30:17 +0100

    Update EvenimentConcert.cpp am adăugat constructorul de copiere si metodele virtuale suprascrise

--- src/EvenimentConcert.cpp
+++ src/EvenimentConcert.cpp
@@ -3,3 +3,13 @@
 EvenimentConcert::EvenimentConcert() : Eveniment(), artist(""), genMuzical("") {}
 
 EvenimentConcert::EvenimentConcert(const std::string &nume, const std::string &data, double pretBaza, const Sala &sala, const std::string &artist, const std::string &genMuzical) : Eveniment(nume, "Concert", data, pretBaza, sala), artist(artist), genMuzical(genMuzical) {}
+
+EvenimentConcert::EvenimentConcert(const EvenimentConcert &other) : Eveniment(other), artist(other.artist), genMuzical(other.genMuzical) {}
+
+double EvenimentConcert::calculeazaVenitMaximTeoretic() const {
+    return Eveniment::calculeazaVenitMaximTeoretic()*1.2;
+}
+std::string EvenimentConcert::descriere() const {
+    return "CONCERT | "+ Eveniment::descriere() + " | artist: " + artist + " | gen: " + genMuzical;
+}
+
