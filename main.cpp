e9127f4d2cad4528bd6075d64bfe41ee685ece8c
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-03 19:12:07 +0100

    Update main.cpp am creat evenimentele

--- main.cpp
+++ main.cpp
@@ -19,5 +19,10 @@ int main() {
     Sala salaFilm("Film", 5, 10);
     salaFilm.initializareLocuriVIP(3);
 
+    auto* evTeatru = new EvenimentTeatru("Aici nu moare nimeni niciodata", "10-02-2026", 50.0, salaTeatru, "Bogdan Olteanu", "Trupa IELE");
+    auto* evConcert = new EvenimentConcert("Concert Vama", "14-01-2026", 80.0, salaConcert, "Vama", "Pop-Rock");
+    auto* evFilm = new EvenimentFilm("Avatar foc si cenusa", "10-01-2026", 40.0, salaFilm, "James Cameron", "AP-12");
+    
+
     return 0;
 }
