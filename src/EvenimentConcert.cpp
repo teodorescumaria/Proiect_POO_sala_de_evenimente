a47e774c90b421215add9cc926661908c2f2246e
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-03 10:33:25 +0100

    Update EvenimentConcert.cpp Am adaugat getteri si operatorul =

--- src/EvenimentConcert.cpp
+++ src/EvenimentConcert.cpp
@@ -1,10 +1,24 @@
 #include "EvenimentConcert.h"
 
 EvenimentConcert::EvenimentConcert() : Eveniment(), artist(""), genMuzical("") {}
-
 EvenimentConcert::EvenimentConcert(const std::string &nume, const std::string &data, double pretBaza, const Sala &sala, const std::string &artist, const std::string &genMuzical) : Eveniment(nume, "Concert", data, pretBaza, sala), artist(artist), genMuzical(genMuzical) {}
-
 EvenimentConcert::EvenimentConcert(const EvenimentConcert &other) : Eveniment(other), artist(other.artist), genMuzical(other.genMuzical) {}
+
+EvenimentConcert& EvenimentConcert::operator=(const EvenimentConcert &other) {
+    if (this != &other) {
+        Eveniment::operator=(other);
+        artist = other.artist;
+        genMuzical = other.genMuzical;
+    }
+    return *this;
+}
+
+const std::string& EvenimentConcert::getArtist() const {
+    return artist;
+}
+const std::string& EvenimentConcert::getGenMuzical() const {
+    return genMuzical;
+}
 
 double EvenimentConcert::calculeazaVenitMaximTeoretic() const {
     return Eveniment::calculeazaVenitMaximTeoretic()*1.2;
