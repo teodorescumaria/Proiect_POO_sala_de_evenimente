2df5ecd8d8985cce8bbcd80fdeb23f83ee8a3d9d
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-03 10:43:50 +0100

    Update EvenimentFilm.h am adăugat destructorul si getteri

--- include/EvenimentFilm.h
+++ include/EvenimentFilm.h
@@ -12,5 +12,9 @@ public:
     EvenimentFilm();
     EvenimentFilm(const std::string& nume, const std::string& data, double pretBaza, const Sala& sala, const std::string& regizor, const std::string& raiting);
     EvenimentFilm(const EvenimentFilm& other);
+
+    ~EvenimentFilm() override = default;
+    const std::string& getRegizor() const;
+    const std::string& getRating() const;
 };
 #endif
