7aadcb34615b0932b8a7f1203ec47baf56035b41
teodorescumaria <teodorescumaria2005@gmail.com>
2026-01-07 20:31:15 +0200

    Update main.cpp am adaugat citire si afisare din fisier

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+//
+#include <fstream>
+//
+
 
 #include "Persoana.h"
 #include "Sala.h"
@@ -10,7 +14,20 @@
 
 int main() {
 
-    std::cout<<"=== Sistem de vanzare bilete la evenimente === \n";
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+
+    if (!fin.is_open()) {
+        std::cout<<"Nu s-a putut deschide fisierul\n";
+        return 1;
+    }
+    if (!fout.is_open()) {
+        std::cout<<"Nu s-a putut deschide fisierul\n";
+        return 1;
+    }
+
+    fout<<"=== Sistem de vanzare bilete la evenimente === \n";
+    fout<<"\n";
 
     Sala salaTeatru("Teatru", 5, 10);
     salaTeatru.initializareLocuriVIP(2);
@@ -19,116 +36,124 @@ int main() {
     Sala salaFilm("Film", 5, 10);
     salaFilm.initializareLocuriVIP(3);
 
-    auto* evTeatru = new EvenimentTeatru("Aici nu moare nimeni niciodata", "10-02-2026", 50.0, salaTeatru, "Bogdan Olteanu", "Trupa IELE");
-    auto* evConcert = new EvenimentConcert("Concert Vama", "14-01-2026", 80.0, salaConcert, "Vama", "Pop-Rock");
-    auto* evFilm = new EvenimentFilm("Avatar foc si cenusa", "10-01-2026", 40.0, salaFilm, "James Cameron", "AP-12");
+    auto* evTeatru = new EvenimentTeatru("Iona", "10-02-2026", 50.0, salaTeatru, "Bogdan Olteanu", "Trupa IELE");
+    auto* evConcert = new EvenimentConcert("Vama", "14-01-2026", 80.0, salaConcert, "Vama", "Pop-Rock");
+    auto* evFilm = new EvenimentFilm("Avatar", "10-01-2026", 40.0, salaFilm, "James Cameron", "AP-12");
 
      ManagerEvenimente manager;
     manager.adaugaEveniment(evTeatru);
     manager.adaugaEveniment(evConcert);
     manager.adaugaEveniment(evFilm);
 
-    std::cout<<"Numar total de evenimente create: "<<Eveniment::getNrEvenimente()<<"\n";
-    std::cout<<"Detalii evenimente initiale: \n";
-    std::cout<<"Teatru: "<<evTeatru->descriere()<<" | trupa: "<<evTeatru->getTrupa()<<"\n";
-    std::cout<<"Concert: "<<evConcert->getArtist()<<" | gen: "<<evConcert->getGenMuzical() << "\n";
-    std::cout<< "Film: regizor " << evFilm->getRegizor() << " | rating: " << evFilm->getRating()<<"\n";
+    fout<<"Numar total de evenimente create: "<<Eveniment::getNrEvenimente()<<"\n";
+    fout<<"\n";
+
+    fout<<"Detalii evenimente initiale: \n";
+    fout<<evTeatru->descriere()<<" | trupa: "<<evTeatru->getTrupa()<<"\n";
+    fout<<evConcert->descriere()<<" | artist: "<<evConcert->getArtist()<<" | gen: "<<evConcert->getGenMuzical() << "\n";
+    fout<<evFilm->descriere()<<" | regizor " << evFilm->getRegizor() << " | rating: " << evFilm->getRating()<<"\n";
 
 
     int optiune = -1;
 
     while (true) {
-        std::cout<<"\n === MENIU === \n";
-        std::cout<<"1. Afiseaza evenimente\n";
-        std::cout<<"2. Afiseaza bilete vandute\n";
-        std::cout<<"3. Vinde bilet\n";
-        std::cout<<"4. Afiseaza incasari totale\n";
-        std::cout<<"5. Afiseaza locurile pentru un eveniment\n";
-        std::cout<<"0. Iesire\n";
-        std::cout<<"Alegeti optiunea: ";
-        std::cin>>optiune;
+        fout<<"\n === MENIU === \n";
+        fout<<"1. Afiseaza evenimente\n";
+        fout<<"2. Afiseaza bilete vandute\n";
+        fout<<"3. Vinde bilet\n";
+        fout<<"4. Afiseaza incasari totale\n";
+        fout<<"5. Afiseaza locurile pentru un eveniment\n";
+        fout<<"0. Iesire\n";
+        fout<<"Alegeti optiunea: \n";
+        fin>>optiune;
 
-        if (!std::cin) {
-            std::cout<<"Eroare la citire \n";
+        if (!fin) {
+            fout<<"Eroare la citire \n";
             break;
         }
         if (optiune == 0) {
-            std::cout<<"LA REVEDER! \n";
+            fout<<"LA REVEDERE! \n";
             break;
         } else if (optiune == 1) {
-            manager.afiseazaEvenimente();
+            manager.afiseazaEvenimente(fout);
         } else if (optiune == 2) {
-            manager.afiseazaBilete();
+            manager.afiseazaBilete(fout);
         } else if (optiune == 3) {
-            std::cout<<"\n --- Vanzare bilet ---\n";
-            std::cout<<"Introdu numele evenimentului (Aici_nu moare_nimeni_niciodata / ConcertVama / AvatarFocSiCenusa): ";
+            fout<<"\n --- Vanzare bilet ---\n";
+            fout<<"Introdu numele evenimentului (Iona / Vama / Avatar): \n";
             std::string numeEveniment;
-            std::cin>>numeEveniment;
+            fin>>numeEveniment;
 
             Eveniment* eveniment = manager.gasesteEvenimentDupaNume(numeEveniment);
             if (!eveniment) {
-                std::cout<<"Evenimentul nu a fost gasit. \n";
+                fout<<"Evenimentul nu a fost gasit. \n";
                 continue;
             }
 
-            std::cout<<"Nume persoana: ";
-            std::string numePersoana;
-            std::cin>>numePersoana;
+            fout<<"\nHarta locurilor\n";
+            eveniment->getSala().afisareLocuri(fout);
+            fout<<"\n(- loc liber, V loc Vip, X loc ocupat)\n";
+            fout<<"\n";
 
-            std::cout<<"Anul nasterii persoanei: ";
+            fout<<"Nume persoana: \n";
+            std::string numePersoana;
+            fin>>std::ws; //ca sa sara peste new line
+            std::getline(fin, numePersoana);
+
+            fout<<"Anul nasterii persoanei: \n";
             int anulNasterii;
-            std::cin>>anulNasterii;
+            fin>>anulNasterii;
 
             Persoana p(numePersoana, anulNasterii);
 
-            std::cout<<"Tip bilet (1 = Standard, 2 = Redus, 3 = VIP): ";
+            fout<<"Tip bilet (1 = Standard, 2 = Redus, 3 = VIP): \n";
             int tipBilet;
-            std::cin>>tipBilet;
+            fin>>tipBilet;
 
-            std::cout<<"Rand: ";
+            fout<<"Rand: \n";
             int rand;
-            std::cin>>rand;
-            std::cout<<"Coloana: ";
+            fin>>rand;
+            fout<<"Coloana: \n";
             int coloana;
-            std::cin>>coloana;
+            fin>>coloana;
 
             const Bilet* b = nullptr;
 
             if (tipBilet == 1) {
-                b = manager.vindeBiletStandard(p, *eveniment, rand, coloana);
+                b = manager.vindeBiletStandard(p, *eveniment, rand, coloana, fout);
             } else if (tipBilet == 2) {
-                b = manager.vindeBiletReducere(p, *eveniment, rand, coloana);
+                b = manager.vindeBiletReducere(p, *eveniment, rand, coloana, fout);
             } else if (tipBilet == 3) {
                 int pop, baut;
-                std::cout<<"Popcorn inclus? (1 = da, 0 = nu): ";
-                std::cin>>pop;
-                std::cout<<"Bautura inclusa? (1 = da, 0 = nu): ";
-                std::cin>>baut;
-                b = manager.vindeBiletVip(p, *eveniment, rand, coloana, pop, baut);
+                fout<<"Popcorn inclus? (1 = da, 0 = nu): \n";
+                fin>>pop;
+                fout<<"Bautura inclusa? (1 = da, 0 = nu): \n";
+                fin>>baut;
+                b = manager.vindeBiletVip(p, *eveniment, rand, coloana, pop, baut, fout);
             } else {
-                std::cout<<"Tip bilet invalid\n";
+                fout<<"Tip bilet invalid\n";
             }
             if (b!=nullptr) {
-                std::cout<<"Bilet vandut: "<<b->descriere()<<" | pret final: "<<b->calculeazaPret()<<"\n";
+                fout<<"Bilet vandut: "<<b->descriere()<<" | pret final: "<<b->calculeazaPret()<<"\n";
             }
         } else if (optiune == 4) {
-            std::cout<<"Incasari totale: "<<manager.calculeazaIncasariTotale()<<"lei \n";
+            fout<<"Incasari totale: "<<manager.calculeazaIncasariTotale()<<"lei \n";
         } else if (optiune == 5) {
-            std::cout<<"Introdu numele evenimentului: ";
+            fout<<"Introdu numele evenimentului: \n";
             std::string numeEv;
-            std::cin >> numeEv;
+            fin >> numeEv;
             Eveniment* ev = manager.gasesteEvenimentDupaNume(numeEv);
             if(!ev) {
-                std::cout<<"Evenimentul nu a fost gasit. \n";
+                fout<<"Evenimentul nu a fost gasit. \n";
             } else {
-                ev->getSala().afisareLocuri();
+                ev->getSala().afisareLocuri(fout);
             }
         }
         else {
-                std::cout<<"Optiune invalida\n";
+                fout<<"Optiune invalida\n";
             }
 
         }
-    
+
     return 0;
 }
